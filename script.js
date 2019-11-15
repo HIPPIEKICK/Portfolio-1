c50c53df0c8a3e7e644679c15863366a8dc4b7b2
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2019-11-15 14:07:35 +0100

    Reboot

---
+++ script.js
@@ -0,0 +1,30 @@
+const projectButton1 = document.getElementById("readMore1")
+const projectButton2 = document.getElementById("readMore2")
+const projectButton3 = document.getElementById("readMore3")
+
+const extraText1 = document.getElementById("moreText1")
+const extraText2 = document.getElementById("moreText2")
+const extraText3 = document.getElementById("moreText3")
+
+const expandText = () => {
+<<<<<<< HEAD
+  if (extraText1.style.display === "none") {
+    extraText1.style.display = "inline"
+    projectButton1.innerHTML = "<<"
+
+  } else {
+    extraText1.style.display = "none"
+    projectButton1.innerHTML = ">>"
+  }
+}
+
+// const expandText = () => {
+//   this.yadayada.toggle("expanded")
+// }
+
+// document.getElementById("readMore1").onclick = expandText()
+=======
+  extraText1.style.display === "block"
+}
+
+>>>>>>> e077a41... Added javascript-file and started styling the projects
